8346160872c5b4c0ebb251eb4f37786debe2e071
Carlos Precioso <kalprestito@gmail.com>
2018-12-17 04:14:30 +0100

    Strict types for Packager

--- src/packager.ts
+++ src/packager.ts
@@ -1,20 +1,21 @@
 import CSSPackager = require("parcel-bundler/src/packagers/CSSPackager")
+import CSSAsset = require("parcel-bundler/src/assets/CSSAsset")
 import PurgeCSS = require("purgecss")
 
 class PurgeCSSPackager extends CSSPackager {
-  async addAsset(asset) {
+  async addAsset(asset: CSSAsset) {
     if (this.options.minify) {
       console.log("minify")
-      const config = await asset.parentBundle.entryAsset.getConfig(
+      const config = (await asset.parentBundle!.entryAsset.getConfig(
         ["purgecss.config.js"],
         { packageKey: "purgecss" }
-      )
+      )) as PurgeCSS.Options
 
       if (config) {
         console.log("config")
-        asset.generated.css = new PurgeCSS({
+        asset.generated!.css = new PurgeCSS({
           ...config,
-          css: [{ extension: "css", raw: asset.generated.css }]
+          css: [{ extension: "css", raw: asset.generated!.css }]
         }).purge()[0].css
       }
     }
--- types/parcel-bundler.d.ts
+++ types/parcel-bundler.d.ts
@@ -1,22 +1,80 @@
-declare module "parcel-bundler/src/Asset" {
-  class Asset {
-    name: string
-    options: { minify: boolean }
-    getConfig(filenames: string[], { packageKey: string }): Promise<any>
-    transform(): Promise<void>
-    pretransform(): Promise<void>
-    parseIfNeeded(): Promise<void>
+declare module "parcel-bundler/src/Bundle" {
+  import Asset = require("parcel-bundler/src/Asset")
+
+  class Bundle<A extends Asset = Asset> {
+    public entryAsset: A
   }
+
+  export = Bundle
+}
+
+declare module "parcel-bundler/src/Asset" {
+  import Bundle = require("parcel-bundler/src/Bundle")
+
+  class Asset {
+    public parentBundle?: Bundle<this>
+    public generated?: { [type: string]: string }
+
+    getConfig(
+      filenames: string[],
+      opts?: { packageKey?: string; path?: string; load: false }
+    ): Promise<string>
+    getConfig(
+      filenames: string[],
+      opts?: { packageKey?: string; path?: string; load?: true }
+    ): Promise<any>
+  }
+
   export = Asset
 }
 
 declare module "parcel-bundler/src/assets/CSSAsset" {
   import Asset = require("parcel-bundler/src/Asset")
-  import postcss = require("postcss")
 
   class CSSAsset extends Asset {
-    getCSSAst(): postcss.Result
-    ast: { css: string; dirty: boolean }
+    public generated?: { css: string }
   }
+
   export = CSSAsset
+}
+
+declare module "parcel-bundler/src/packagers/Packager" {
+  import { WriteStream } from "fs"
+  import * as Bundler from "parcel-bundler"
+  import Bundle = require("parcel-bundler/src/Bundle")
+  import Asset = require("parcel-bundler/src/Asset")
+
+  abstract class Packager<A extends Asset = Asset> {
+    public bundle: Bundle<A>
+    public bundler: Bundler
+    public options: Bundler.ParcelOptions
+
+    protected dest?: WriteStream & {
+      write(chunk: any): Promise<void>
+      end(): Promise<void>
+    }
+
+    static shouldAddAsset(): boolean
+
+    constructor(bundle: Bundle<A>, bundler: Bundler)
+    setup(): Promise<void>
+    write(string: string): Promise<void>
+    start(): Promise<void>
+    abstract addAsset(asset: A): Promise<void>
+    getSize(): number
+    end(): Promise<void>
+  }
+
+  export = Packager
+}
+
+declare module "parcel-bundler/src/packagers/CSSPackager" {
+  import Packager = require("parcel-bundler/src/packagers/Packager")
+  import CSSAsset = require("parcel-bundler/src/assets/CSSAsset")
+
+  class CSSPackager extends Packager<CSSAsset> {
+    addAsset(asset: CSSAsset): Promise<void>
+  }
+
+  export = CSSPackager
 }
