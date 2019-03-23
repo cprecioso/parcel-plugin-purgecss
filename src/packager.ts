eea45226785fbfa4bfa84bf3fce6c54fb22d78e2
Brian Bugh <brainbag@gmail.com>
2019-03-23 11:06:58 -0500

    Remove debugging console.log

--- src/packager.ts
+++ src/packager.ts
@@ -5,14 +5,12 @@ import PurgeCSS = require("purgecss")
 class PurgeCSSPackager extends CSSPackager {
   async addAsset(asset: CSSAsset) {
     if (this.options.minify) {
-      console.log("minify")
       const config = (await asset.parentBundle!.entryAsset.getConfig(
         ["purgecss.config.js"],
         { packageKey: "purgecss" }
       )) as PurgeCSS.Options
 
       if (config) {
-        console.log("config")
         asset.generated!.css = new PurgeCSS({
           ...config,
           css: [{ extension: "css", raw: asset.generated!.css }]
